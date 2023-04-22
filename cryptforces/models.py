cfe302d59e25ee7c283862394d76032e3534dec9
Rachit Khurana <70265590+notnotrachit@users.noreply.github.com>
2023-04-22 12:34:04 +0530

    added user solving backend

--- cryptforces/models.py
+++ cryptforces/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from cryptic.models import Question
 
 class CustomUser(AbstractUser):
     points = models.IntegerField(default=0)
+    question_solved = models.ManyToManyField(Question, blank=True)
--- cryptic/views.py
+++ cryptic/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from cryptic.models import Question
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+@login_required
 # Create your views here.
 def problem(request, problem_id):
     if request.method == 'POST':
         problem = Question.objects.get(id=problem_id)
         answer = request.POST['answer']
         if problem.answer == answer:
+            user = request.user
+            user.points += problem.points
+            if problem not in user.question_solved.all():
+                user.question_solved.add(problem)
+            user.save()
             return render(request, 'problem.html', {'problem': problem, 'toast': "Correct"})
         else:
             return render(request, 'problem.html', {'problem': problem, 'toast': "Incorrect"})
@@ -14,6 +22,7 @@ def problem(request, problem_id):
     return render(request, 'problem.html', {'problem': problem})
 
 
+@login_required
 def all_problems(request):
     problems = Question.objects.all()
     return render(request, 'all_problems.html', {'problems': problems})
